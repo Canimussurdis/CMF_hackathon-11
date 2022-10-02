7327680c06b2a69eff079fe9cdc2c3b268c3d446
Евгений Чубчев <evgenijcubcev@MacBook-Pro-Evgenij.local>
2022-10-02 20:34:08 +0300

    Добавлена функция, расчитывающая прирост суммарного числа заказов за день, и функция, создающая признаки со сдвигом

--- features.py
+++ features.py
@@ -1,7 +1,7 @@
 # Файл, в который можно складывать код для написания всех фичей
 import pandas as pd
 import numpy as np
-from fastai.tabular.core import add_datepart
+# from fastai.tabular.core import add_datepart
 
 
 def daily_order_counts(orders):
@@ -28,14 +28,15 @@ def date_features(df):
             df - dataframe with dates, delivery areas id, and order counts
         Return
             df_proc - dataframe with dates, delivery areas id, order counts,
-            and date features Month, Day, Dayofweek, Is_month_end, Is_month_start,
-            Is_quarter_end,	Is_quarter_start
+            and date features Month, Day, Dayofweek, Is_month_end,
+            Is_month_start, Is_quarter_end,	Is_quarter_start
     """
     df_proc = df.copy()
     add_datepart(df_proc, 'date', drop=False)
-    df_proc.drop(['Year', 'Dayofyear', 'Elapsed', 'Is_year_end', 'Is_year_start', 'Week'],
-             axis=1,
-             inplace=True)
+    df_proc.drop(['Year', 'Dayofyear', 'Elapsed', 'Is_year_end',
+                  'Is_year_start', 'Week'],
+                 axis=1,
+                inplace=True)
     df_proc['Is_month_end'] = df_proc['Is_month_end'].astype(int)
     df_proc['Is_month_start'] = df_proc['Is_month_start'].astype(int)
     df_proc['Is_quarter_end'] = df_proc['Is_quarter_end'].astype(int)
