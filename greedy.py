7a5abc21a872704c72d1f5e06b3cd308c6ee5346
Canimussurdis <76573311+Canimussurdis@users.noreply.github.com>
2022-10-06 00:40:16 +0300

    Обновлен жадный перебор

--- greedy.py
+++ greedy.py
@@ -26,12 +26,13 @@ def is_left_hours(workload):
 
 shift_hours = [4, 5, 6, 7, 8]
 shift_hours.sort() # Опционально, если список изначально не сортирован
-workload = [1, 5, 3, 1, 2, 1, 3, 5, 6, 3, 1]
-while is_left_hours(workload):
+left_hours = [1, 5, 3, 1, 2, 1, 3, 5, 6, 3, 1]
+need_workload = left_hours.copy()
+while is_left_hours(left_hours):
     best_shift = 0
-    best_pos, best_hour = get_greed(shift_hours[0], workload)
+    best_pos, best_hour = get_greed(shift_hours[0], left_hours)
     for current_shift, hour in enumerate(shift_hours[1:]):
-        current_pos, current_hour = get_greed(hour, workload)
+        current_pos, current_hour = get_greed(hour, left_hours)
         if best_hour < current_hour:
             best_pos = current_pos
             best_hour = current_hour
@@ -39,9 +40,13 @@ while is_left_hours(workload):
     # Ставим смену
     duration = shift_hours[best_shift]
     for i in range(best_pos, best_pos+duration):
-        workload[i] -= 1
-    print(*workload)
+        left_hours[i] -= 1
+    print(*left_hours)
     print(f"Была поставлена смена на {duration} часов с {best_pos} по {best_pos+duration-1} часов")
-print(f"Количество лишних часов: {-sum(workload)}")
+print(f"Количество лишних часов: {-sum(left_hours)}")
+
+current_workload = [need_workload[i] - left_hours[i] for i in range(len(need_workload))]
+print(*need_workload) # Синий график
+print(*current_workload) # Оранжевый график
 
 
